9fa17892d9cb0525bd9f28ecc19b82444c1cf7ae
annkupi <anna-kupina@yandex.ru>
2017-04-19 12:56:24 +0300

    -delete useless things

--- generated/redirector.php
+++ generated/redirector.php
@@ -9,7 +9,6 @@ $user = "root"; //TODO: enter current parameters
 $password = "WVClanguniver"; //TODO: enter current parameters
 
     //Connect DB
-
 $dbLink = mysqli_connect($host, $user, $password, $dbName) or trigger_error(mysql_error(),E_USER_ERROR);
 
 mysqli_query($dbLink, "SET NAMES 'utf8'")                                      or die(mysql_error());
@@ -18,9 +17,6 @@ mysqli_query($dbLink, "SET SESSION collation_connection = 'utf8_general_ci'")  o
 
 mysqli_select_db($dbLink, $dbName);
 
-//echo "qwe<br>";
-//echo "dbLink " . mysqli_get_host_info($dbLink) . "<br>";
-
     //DB queries
 
 $shortURI = $_GET['shortUrl'];
@@ -28,15 +24,6 @@ $query = "SELECT `longLink` FROM `link` WHERE `shortLink`='$shortURI'";
 $mysqlResult = mysqli_query($dbLink, $query);
 $longURI = mysqli_fetch_array($mysqlResult)['longLink'];
 
-//echo "shortURI = ";
-//echo $shortURI . "<br>";
-//echo "longURI = ";
-//echo $longURI . "<br>";
-
 header('Location: ' . $longURI);
-//header('Location: https://www.yandex.ru/');
-
-//echo 'redirector<br>';
-//echo "shortURI = " . $_GET['shortUrl'];
 
 ?>
